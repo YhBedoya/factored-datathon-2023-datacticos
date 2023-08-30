70b49fc92be6c1e5174ae28d343b5d0955349e70
Yhorman Bedoya <84676079+YhBedoya@users.noreply.github.com>
2023-08-30 18:20:24 -0500

    Update 1 - 🎯  Amazon Review Analyzer.py

--- Frontend/pages/1 - 🎯  Amazon Review Analyzer.py
+++ Frontend/pages/1 - 🎯  Amazon Review Analyzer.py
@@ -14,7 +14,7 @@ openai.api_key = "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"
 
 
 #Select path to all data
-path_to_master_data='/home/sites/reviews_master_parquet_3/' # deployment path
+path_to_master_data='/home/sites/reviews_master_parquet/' # deployment path
 #path_to_master_data='/home/yhbedoya/Datathon/reviews_master_parquet/' # local path
 #path_to_master_data='./master_data/reviews_master_parquet/' # local path Gabriel
 
